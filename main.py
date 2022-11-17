d19cdfdc4df3b950dff368ae50e50a3b347c6171
goykh <geronymo.fuhrmann@gmail.com>
2022-11-17 22:45:13 +0100

    cunt

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Query, Body
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -25,6 +26,18 @@ users = [{"jmeno": "Petr",
          ]
 
 
+class User(BaseModel):
+    user: str
+
+
+class Transaction(BaseModel):
+    dluznik: str
+    veritel: str
+    suma: float
+
+
+class UserList(BaseModel):
+    uzivatele: list[str] = Query()
 def get_sum_of_amounts():
     for i, j in enumerate(users):
         amount = sum(users[i]['dluzi'].values()) + sum(users[i]['dluzi_mu'].values())
@@ -32,13 +45,12 @@ def get_sum_of_amounts():
 
 
 @app.get('/users')
-async def get_users(users_list: list[str] | None = None):
+async def get_users(uzivatele: dict = Body()):
     get_sum_of_amounts()
-    # TODO: dis no work, need to fix
-    if users_list:
+    if uzivatele:
         result = []
         for i, j in enumerate(users):
-            if users[i]['jmeno'] in users_list:
+            if users[i]['jmeno'] in uzivatele:
                 result.append(users[i])
         return result.sort()
     else:
@@ -46,39 +58,44 @@ async def get_users(users_list: list[str] | None = None):
 
 
 @app.post('/add')
-async def add_new_user(user: str):
-    #TODO: need to add if statement to stop duplicate names
-    users.append({
-        'jmeno': user,
+async def add_new_user(user: User):
+    for i, j in enumerate(users):
+        if j['jmeno'] == user.user:
+            raise HTTPException(status_code=400, detail="Jméno už je v seznamu.")
+    new_user = {
+        'jmeno': user.user,
         'dluzi': {},
         'dluzi_mu': {},
         'suma': ""
-    })
-    print(users)
-    return {'user': user}
+    }
+    users.append(new_user)
+    return new_user
 
 
 @app.post('/transaction')
-async def make_transaction(creditor: str, debtor: str, amount: float):
+async def make_transaction(transaction: Transaction):
     # adding data to debtor
+    names_of_users = [i['jmeno'] for i in users]
+    if transaction.dluznik not in names_of_users or transaction.veritel not in names_of_users:
+        raise HTTPException(status_code=400, detail="Zadali jste jméno, které se v seznamu nenachází.")
     for i, j in enumerate(users):
-        if users[i].get('jmeno') == debtor:
-            if creditor in users[i].get('dluzi'):
-                users[i]['dluzi'][creditor] += amount
+        if users[i]['jmeno'] == transaction.dluznik:
+            if transaction.veritel in users[i]['dluzi']:
+                users[i]['dluzi'][transaction.veritel] += transaction.suma
             else:
-                users[i]['dluzi'][creditor] = amount
+                users[i]['dluzi'][transaction.veritel] = transaction.suma
 
     # adding data to creditor
     for i, j in enumerate(users):
-        if users[i].get('jmeno') == creditor:
-            if debtor in users[i].get('dluzi_mu'):
-                users[i]['dluzi_mu'][debtor] += amount
+        if users[i]['jmeno'] == transaction.veritel:
+            if transaction.dluznik in users[i]['dluzi_mu']:
+                users[i]['dluzi_mu'][transaction.dluznik] += transaction.suma
             else:
-                users[i]['dluzi_mu'][debtor] = amount
+                users[i]['dluzi_mu'][transaction.dluznik] = transaction.suma
     get_sum_of_amounts()
     result = []
     for i, j in enumerate(users):
-        if users[i]['jmeno'] in [debtor, creditor]:
+        if users[i]['jmeno'] in [transaction.dluznik, transaction.veritel]:
             result.append(users[i])
 
     return result
